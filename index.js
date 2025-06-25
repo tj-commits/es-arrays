2baf31ed6e980aeadc8c77ab919f6ec6e56ad050
Fox <thefoxjones@gmail.com>
2025-06-25 14:49:26 -0500

    some changes

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 var GetIntrinsic = require("get-intrinsic");
-var __global = require("global/window");
-var hasOwn = require("hasown");
-var $Float16Array = require("@petamoriken/float16").Float16Array;
+var typedArrays = require("available-typed-arrays");
 
-var arrayIntrinsicNames = ["Array", "TypedArray", "Int8Array", "Int16Array", "Int32Array", "Uint8Array", "Uint8ClampedArray", "Uint16Array", "Uint32Array", "Float16Array", "Float32Array", "Float64Array", "BigInt64Array", "BigUint64Array"];
+var arrayIntrinsicNames = ["Array", "TypedArray"].concat(typedArrays);
 
 function getCachedArrayIntrinsics() {
   var result = [];
   for (var i = 0; i < arrayIntrinsicNames.length; i++) {
-    var intrinsicName = arrayIntrinsicNames[i];
-    var intrinsic;
-    if (intrinsicName === "Float16Array" && !hasOwn(__global, "Float16Array")) {
-      intrinsic = $Float16Array;
-    } else {
-      intrinsic = GetIntrinsic("%" + intrinsicName + "%");
-    }
-    result.push(intrinsic);
+    result.push(GetIntrinsic("%" + arrayIntrinsicNames[i] + "%"));
   }
   return result;
 }
